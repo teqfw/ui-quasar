9f0d5544ac9f05095e186701d4b8bf9615bc90f7
Alex Gusev <alex@flancer64.com>
2021-03-13 14:22:36 +0200

    MF-28 i18n for existing components

--- src/Defaults.mjs
+++ src/Defaults.mjs
@@ -1,5 +1,6 @@
 export default class TeqFw_Ui_Quasar_Defaults {
-    BACK_REALM = 'uiQuasar';  // realm for API services ('/api/project/...') and CLI commands ('project-...')
+    BACK_REALM = 'uiQuasar'; // realm for API services ('/api/project/...') and CLI commands ('project-...')
+    DI_QUASAR = 'quasar'; // DI container label for Quasar lib.
     /** @type {TeqFw_Core_App_Defaults} */
     MOD_CORE;
 
