e2b8778c41786b48d6139fbffe1d8889e4986eb8
Alex Gusev <alex@flancer64.com>
2021-06-23 10:23:31 +0300

    WF-453 Review core plugin

--- src/Defaults.mjs
+++ src/Defaults.mjs
@@ -1,12 +1,12 @@
 export default class TeqFw_Ui_Quasar_Defaults {
     BACK_REALM = 'uiQuasar'; // realm for API services ('/api/project/...') and CLI commands ('project-...')
     DI_QUASAR = 'uiQuasar'; // DI container label for Quasar lib.
-    /** @type {TeqFw_Core_App_Defaults} */
+    /** @type {TeqFw_Core_Defaults} */
     MOD_CORE;
 
     constructor(spec) {
-        /** @type {TeqFw_Core_App_Defaults} */
-        this.MOD_CORE = spec['TeqFw_Core_App_Defaults$'];    // pin 'core' defaults
+        /** @type {TeqFw_Core_Defaults} */
+        this.MOD_CORE = spec['TeqFw_Core_Defaults$'];    // pin 'core' defaults
         Object.freeze(this);
     }
 }
