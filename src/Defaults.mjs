9b15378c4b92b4a2d90ff913a40b9b2f08e96ba8
Alex Gusev <alex@flancer64.com>
2021-07-01 16:37:46 +0300

    WF-457 Create @teqfw/web plugin

--- src/Defaults.mjs
+++ src/Defaults.mjs
@@ -1,12 +1,8 @@
 export default class TeqFw_Ui_Quasar_Defaults {
     BACK_REALM = 'uiQuasar'; // realm for API services ('/api/project/...') and CLI commands ('project-...')
     DI_QUASAR = 'uiQuasar'; // DI container label for Quasar lib.
-    /** @type {TeqFw_Core_Defaults} */
-    MOD_CORE;
 
-    constructor(spec) {
-        /** @type {TeqFw_Core_Defaults} */
-        // this.MOD_CORE = spec['TeqFw_Core_Defaults$'];    // pin 'core' defaults
+    constructor() {
         Object.freeze(this);
     }
 }
