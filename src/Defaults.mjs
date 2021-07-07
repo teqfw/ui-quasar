7d51e3602d859c4ac0819d3909fde970deac42e4
Alex Gusev <alex@flancer64.com>
2021-07-07 17:35:15 +0300

    WF-462 Review DEFs modules

--- src/Defaults.mjs
+++
@@ -1,8 +0,0 @@
-export default class TeqFw_Ui_Quasar_Defaults {
-    BACK_REALM = 'uiQuasar'; // realm for API services ('/api/project/...') and CLI commands ('project-...')
-    DI_QUASAR = 'uiQuasar'; // DI container label for Quasar lib.
-
-    constructor() {
-        Object.freeze(this);
-    }
-}
---
+++ src/Front/Defaults.mjs
@@ -0,0 +1,11 @@
+/**
+ * Plugin constants (hardcoded configuration) for frontend code.
+ */
+export default class TeqFw_Ui_Quasar_Front_Defaults {
+
+    DI_QUASAR = 'quasarLib'; // DI container key for Quasar lib.
+
+    constructor() {
+        Object.freeze(this);
+    }
+}
