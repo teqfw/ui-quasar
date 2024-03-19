126d7b260805ebc0f4f8139f5f8d3cc6ca4c7070
Alex Gusev <alex@flancer64.com>
2024-03-19 18:03:02 +0200

    LP-8 Update `Fl32_Auth_Front_Ext_Nacl`

--- src/Front/Ext.mjs
+++ src/Front/Ext.mjs
@@ -3,40 +3,8 @@
  *
  * @namespace TeqFw_Ui_Quasar_Front_Ext
  */
-
-// MODULE'S FUNCS
-/**
- * Load UMD script from the back and execute it.
- * @param {string} url
- * @return {Promise}
- */
-async function loadUmd(url) {
-    return new Promise((resolve, reject) => {
-        // Create a script element
-        const script = document.createElement('script');
-        script.src = url;
-        script.onload = () => resolve();
-        script.onerror = (error) => reject(error);
-        document.head.appendChild(script);
-    });
-}
-
-/**
- * Load CSS script from the back and add it to the DOM.
- * @param url
- * @return {Promise}
- */
-async function loadCss(url) {
-    return new Promise((resolve, reject) => {
-        const link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.type = 'text/css';
-        link.href = url;
-        link.onload = () => resolve();
-        link.onerror = (error) => reject(error);
-        document.head.appendChild(link);
-    });
-}
+// MODULE'S IMPORTS
+import {loadCss, loadUmd} from '../../../web/@teqfw/web/js/loaders.mjs';
 
 // MODULE'S MAIN
 if (!window.Quasar) {
